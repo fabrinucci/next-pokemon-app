e1751ed3fed97b8e698fa205d7718892b399d604
Fabrizio <fabrinucci99@gmail.com>
2023-11-09 12:47:07 -0300

    Add static generation to dynamic pages

--- src/app/name/[name]/page.tsx
+++ src/app/name/[name]/page.tsx
@@ -1,17 +1,30 @@
-import type { Pokemon } from 'interfaces';
+import { redirect } from 'next/navigation';
+import type { Pokemon, PokemonListResponse } from 'interfaces';
 import { getPokemonInfo } from 'utils';
 import { PokemonCard } from 'components/pokemon';
+import { pokeApi } from 'api';
+
+interface PageProps {
+  params: {
+    name: string;
+  };
+}
+
+export async function generateStaticParams() {
+  const { data } = await pokeApi.get<PokemonListResponse>('/pokemon?limit=151');
+  const allPokemons: string[] = data.results.map((pokemon) => pokemon.name);
+  return allPokemons.map((name) => ({
+    name,
+  }));
+}
 
 const loadPokemon = async (name: string) => {
   const pokemon = await getPokemonInfo(name);
   return pokemon as Pokemon;
 };
 
-export default async function PokemonPage({
-  params,
-}: {
-  params: { name: string };
-}) {
+export default async function PokemonPage({ params }: PageProps) {
   const pokemon = await loadPokemon(params.name);
+  if (!pokemon) return redirect('/');
   return <PokemonCard pokemon={pokemon} />;
 }
--- src/app/pokemon/[id]/page.tsx
+++ src/app/pokemon/[id]/page.tsx
@@ -1,17 +1,28 @@
 import type { Pokemon } from 'interfaces';
 import { getPokemonInfo } from 'utils';
 import { PokemonCard } from 'components/pokemon';
+import { redirect } from 'next/navigation';
+
+interface PageProps {
+  params: {
+    id: string;
+  };
+}
+
+export async function generateStaticParams() {
+  const allPokemons: string[] = [...Array(151)].map(
+    (_, index) => `${index + 1}`
+  );
+  return allPokemons.map((id) => ({ id }));
+}
 
 const loadPokemon = async (id: string) => {
   const pokemon = await getPokemonInfo(id);
   return pokemon as Pokemon;
 };
 
-export default async function PokemonPage({
-  params,
-}: {
-  params: { id: string };
-}) {
+export default async function PokemonPage({ params }: PageProps) {
   const pokemon = await loadPokemon(params.id);
+  if (!pokemon) return redirect('/');
   return <PokemonCard pokemon={pokemon} />;
 }
