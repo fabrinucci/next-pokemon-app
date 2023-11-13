cf44ef084fa4772f4fac9db01cbf900b2f4c788a
Fabrizio <fabrinucci99@gmail.com>
2023-11-13 14:25:27 -0300

    Fix Navbar home link

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -5,9 +5,10 @@ export const Navbar = () => {
   return (
     <nav className='flex w-full items-center justify-between bg-neutral-800 px-[50px] py-[20px]'>
       <NavLink href='/' legacyBehavior>
-        <Link className='cursor-pointer'>
-          <h2 className='text-5xl'>P</h2>
-          <h3 className='text-2xl font-semibold'>okeBosti</h3>
+        <Link data-testid='home-link' className='cursor-pointer'>
+          <h2 className='text-5xl font-bold'>
+            P<span className='text-2xl font-semibold'>okeBosti</span>
+          </h2>
         </Link>
       </NavLink>
 
