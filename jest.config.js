7cde9a5315bf1cf177eebf3b2f03ada70da33a08
Fabrizio <fabrinucci99@gmail.com>
2023-11-14 11:08:25 -0300

    Add next-router-mock and update jest-dom

--- jest.config.js
+++ jest.config.js
@@ -5,7 +5,6 @@ const createJestConfig = nextJest({ dir: './' });
 const customJestConfig = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   testEnvironment: 'jest-environment-jsdom',
-  preset: 'ts-jest',
 };
 
 module.exports = createJestConfig(customJestConfig);
