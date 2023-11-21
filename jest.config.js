0384da467f0bce038b4ca29c94f14ab8188a4b61
Fabrizio <fabrinucci99@gmail.com>
2023-11-21 11:27:21 -0300

    Add playwright and integrate with unit test

--- jest.config.js
+++ jest.config.js
@@ -5,6 +5,10 @@ const createJestConfig = nextJest({ dir: './' });
 const customJestConfig = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   testEnvironment: 'jest-environment-jsdom',
+  testPathIgnorePatterns: [
+    '<rootDir>/node_modules/',
+    '<rootDir>/src/__tests__/e2e',
+  ],
 };
 
 module.exports = createJestConfig(customJestConfig);
---
+++ playwright.config.ts
@@ -0,0 +1,5 @@
+import { defineConfig } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './src/__tests__/e2e',
+});
