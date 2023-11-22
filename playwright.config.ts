1ad8cf549c672b17cc32ac831ae7e079b1232dc0
Fabrizio <fabrinucci99@gmail.com>
2023-11-22 16:58:47 -0300

    Add e2e app testing

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,4 +2,8 @@ import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
   testDir: './src/__tests__/e2e',
+  use: {
+    baseURL: 'http://localhost:3000/',
+  },
+  fullyParallel: true,
 });
--- src/__tests__/e2e/example.test.ts
+++
@@ -1,6 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-  await expect(page).toHaveTitle(/Playwright/);
-});
