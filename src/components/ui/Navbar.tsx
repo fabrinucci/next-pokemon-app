15d98032b2d4a6a361d868f5a9a1e9065ca34461
Fabrizio <fabrinucci99@gmail.com>
2025-03-25 20:05:06 -0300

    feature: Update Navbar

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -1,24 +1,25 @@
 'use client';
 
 import NavLink from 'next/link';
-import { Link, Spacer } from '@heroui/react';
 
 export const Navbar = () => {
   return (
-    <nav className='flex w-full items-center justify-between bg-neutral-800 px-[50px] py-[20px]'>
-      <NavLink href='/' legacyBehavior>
-        <Link data-testid='home-link' className='cursor-pointer'>
-          <h2 className='text-5xl font-bold'>
-            P<span className='text-2xl font-semibold'>okeBosti</span>
-          </h2>
-        </Link>
+    <nav className='flex w-full items-center justify-between gap-3 bg-neutral-800 px-[50px] py-[20px]'>
+      <NavLink
+        data-testid='home-link'
+        href='/'
+        className='transition-opacity duration-300 hover:opacity-75'
+      >
+        <h2 className='text-5xl font-bold'>
+          P<span className='text-2xl font-semibold'>okeBosti</span>
+        </h2>
       </NavLink>
 
-      <Spacer />
-      <NavLink href='/favorites' legacyBehavior>
-        <Link className='cursor-pointer'>
-          <p className='text-lg'>Favorites</p>
-        </Link>
+      <NavLink
+        href='/favorites'
+        className='transition-opacity duration-300 hover:opacity-75'
+      >
+        <p className='text-lg'>Favorites</p>
       </NavLink>
     </nav>
   );
