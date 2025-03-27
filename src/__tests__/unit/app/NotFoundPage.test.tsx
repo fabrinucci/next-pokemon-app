b94d446aec9570fdb8946a9023efd801772bd530
Fabrizio <fabrinucci99@gmail.com>
2025-03-27 19:40:14 -0300

    test: Add Not found test

---
+++ src/__tests__/unit/app/NotFoundPage.test.tsx
@@ -0,0 +1,26 @@
+import { render, screen } from '@testing-library/react';
+import NotFound from '@/app/not-found';
+
+jest.mock('next/navigation', () => require('next-router-mock'));
+
+describe('NotFound', () => {
+  it('should display the title', () => {
+    render(<NotFound />);
+
+    const title = screen.getByRole('heading');
+    const p = screen.getByRole('paragraph');
+    const linkToHome = screen.getByRole('link');
+
+    expect(title).toBeInTheDocument();
+    expect(p).toBeInTheDocument();
+    expect(linkToHome).toBeInTheDocument();
+    expect(screen.getByAltText('Not found image')).toBeInTheDocument();
+
+    expect(title).toHaveTextContent(/Ups!/i);
+    expect(p).toHaveTextContent(
+      /We can not find the page you are looking for/i
+    );
+
+    expect(linkToHome).toHaveTextContent(/Return home/i);
+  });
+});
