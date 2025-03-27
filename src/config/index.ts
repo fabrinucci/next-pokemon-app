3ace5696058aef37e56f8dbc4e64294b901fecb8
Fabrizio <fabrinucci99@gmail.com>
2025-03-27 13:07:09 -0300

    feat: Add config

---
+++ src/config/index.ts
@@ -0,0 +1,8 @@
+export const config = {
+  DREAM_WORLD_URL:
+    'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world',
+  ARTWORK_URL:
+    'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork',
+  SPRITE_URL:
+    'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon',
+};
