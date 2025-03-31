68017cd46b2db0293d71a5a2ac6784f4ab8f2b36
Fabrizio <fabrinucci99@gmail.com>
2025-03-31 11:31:17 -0300

    style: Change image styles

--- src/components/pokemon/PokemonHomeCard.tsx
+++ src/components/pokemon/PokemonHomeCard.tsx
@@ -13,21 +13,19 @@ export const PokemonHomeCard = ({ pokemon }: PokemonProps) => {
       className='relative rounded-xl bg-zinc-900 transition-colors duration-300 hover:bg-zinc-800'
     >
       <Link href={`/name/${pokemon.name}`} className='h-full w-full'>
-        <div className='after:absolute after:left-0 after:top-12 after:h-[1px] after:w-[100%] after:bg-zinc-700 after:content-[""]'></div>
-
         <div className='p-2.5'>
           <h3 className='text-left text-base font-semibold capitalize sm:text-xl'>
             #{pokemon.id} {pokemon.name}
           </h3>
         </div>
         <div></div>
-        <div className='flex items-center px-2 py-6'>
+        <div className='flex items-center'>
           <Image
-            width={150}
-            height={150}
-            className='h-[150px] w-full'
-            src={`${pokemon.img}`}
-            alt={`${pokemon.name}`}
+            width={100}
+            height={100}
+            className='h-[200px] w-full p-6'
+            src={pokemon.img}
+            alt={pokemon.name}
             priority
           />
         </div>
