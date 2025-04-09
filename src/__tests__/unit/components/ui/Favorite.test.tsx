65bd2ae4ff49203e1946bf733036c0d42650efcb
Fabrizio <fabrinucci99@gmail.com>
2025-04-09 15:55:17 -0300

    feat: Update Favorite  component and tests

--- src/__tests__/unit/components/ui/Favorite.test.tsx
+++ src/__tests__/unit/components/ui/Favorite.test.tsx
@@ -1,7 +1,17 @@
 import { render, screen } from '@testing-library/react';
 import { Favorite } from '@/components/ui';
+import { urlConfig } from '@/config/urlConfig';
 
 jest.mock('next/navigation', () => require('next-router-mock'));
+
+jest.mock('next/image', () => ({
+  __esModule: true,
+  default: (props: any) => {
+    return <img {...props} fill='true' />;
+  },
+}));
+
+const { ARTWORK_URL } = urlConfig;
 
 describe('Favorite', () => {
   it('Should display the card', () => {
@@ -10,9 +20,18 @@ describe('Favorite', () => {
     expect(card).toBeInTheDocument();
   });
 
-  it('Should the card have a link to the pokemon', () => {
-    render(<Favorite pokeId={1} />);
+  it('Should render a link to the correct page', () => {
+    render(<Favorite pokeId={2} />);
     const link = screen.getByRole('link');
     expect(link).toBeInTheDocument();
+    expect(link).toHaveAttribute('href', `/pokemon/2`);
+  });
+
+  it('Should render the favorite pokemon with correct image', () => {
+    render(<Favorite pokeId={5} />);
+    const img = screen.getByRole('img');
+    expect(img).toBeInTheDocument();
+    expect(img).toHaveAttribute('alt', 'Pokemon Favorite #5');
+    expect(img).toHaveAttribute('src', `${ARTWORK_URL}/5.png`);
   });
 });
